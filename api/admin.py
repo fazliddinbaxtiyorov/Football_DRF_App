0a742eb7ca7bea71eeef80e82fdc9cff5350b2e7
Fazliddin Baxtiyorovich <arolboyevfazliddin956@example.com>
2023-11-21 23:28:44 +0500

    Adding: Connecting Models and Create super user

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import FootballModels, FootballScoreModels
 # Register your models here.
+admin.site.register(FootballModels)
+admin.site.register(FootballScoreModels)
