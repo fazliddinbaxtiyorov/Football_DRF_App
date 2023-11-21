c1493d458eb3d124f81729d583b105ce4445940b
Fazliddin Baxtiyorov <137509986+fazliddinbaxtiyorov@users.noreply.github.com>
2023-11-21 21:38:32 +0500

    Adding: All models for project

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+
+class FootballModels(models.Model):
+    teams = models.CharField(max_length=100)
+    competition = models.CharField(max_length=60)
+
+
+class FootballScoreModels(models.Model):
+    teams = models.CharField(max_length=100)
+    score_team1 = models.CharField(max_length=100)
+    score_team2 = models.CharField(max_length=100)
+    last_team1_score = models.CharField(max_length=100)
+    last_team2_score = models.CharField(max_length=100)
+
+
+class LiveLinkModels(models.Model):
+    email = models.EmailField(max_length=120)
