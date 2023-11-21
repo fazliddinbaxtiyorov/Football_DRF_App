986f18f61bf42c3b0f5930a566f42c3f2c53b7ce
Fazliddin Baxtiyorovich <arolboyevfazliddin956@example.com>
2023-11-21 23:26:48 +0500

    Uploading: urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from .views import FootballDatas, FootballView, LinkView
 urlpatterns = [
-    path('', FootballView.as_view()),
-    path('api/scores', FootballDatas.as_view()),
-    path('api/links', LinkView.as_view()),
+    path('api/', FootballView.as_view()),
+    path('api/score', FootballDatas.as_view()),
+    path('api/link', LinkView.as_view()),
     path('api/schema', SpectacularAPIView.as_view(), name='schema'),
     path('api/swagger', SpectacularSwaggerView.as_view(url_name='schema')),
 ]
