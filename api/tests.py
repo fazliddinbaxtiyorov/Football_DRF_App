b8b177c33d651c82b9c94dfcd1f837bc9d214503
Fazliddin Baxtiyorovich <arolboyevfazliddin956@example.com>
2023-11-21 23:39:34 +0500

    Adding: some tests for views.py

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+
 
 # Create your tests here.
+class FootballViewTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_get_teams(self):
+        response = self.client.get('/api/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_datas(self):
+        response = self.client.get('/api/score')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_link(self):
+        response = self.client.get('/api/link')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_get_error(self):
+        response = self.client.get('/api/qwerty')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
