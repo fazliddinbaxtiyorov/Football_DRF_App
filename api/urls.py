e327effd20d60e70c6097c5c941defde2c29f204
Fazliddin Baxtiyorov <137509986+fazliddinbaxtiyorov@users.noreply.github.com>
2023-11-21 21:41:56 +0500

    Uploding: and Adding All urls for project

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from .views import FootballDatas, FootballView, LinkView
 urlpatterns = [
+    path('', FootballView.as_view()),
+    path('api/scores', FootballDatas.as_view()),
+    path('api/links', LinkView.as_view()),
     path('api/schema', SpectacularAPIView.as_view(), name='schema'),
     path('api/swagger', SpectacularSwaggerView.as_view(url_name='schema')),
 ]
